1f9a50e972961ef992dcf77fb2a160a02ac2ada1
ozen-hub <info.ozenhub@gmail.com>
2024-08-28 21:53:19 +0530

    init

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,20 +1,34 @@
 const UserSchema = require('../model/UserSchema');
 const bcrypt = require('bcrypt');
 const wt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 const secret = process.env.SECRET;
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        password: process.env.EMAIL_PASSWORD
+    }
+});
 
 const signup = async (req, resp) => {
     //console.log(req.body);
     try {
 
-        const existingUser = await UserSchema.findOne({email:req.body.email});
 
-        if(existingUser){
+        const existingUser = await UserSchema.findOne({email: req.body.email});
+
+        if (existingUser) {
             return resp.status(400).json({'message': 'user already exists'});
         }
 
-       const hash = await bcrypt.hash(req.body.password,10);
+
+        const hash = await bcrypt.hash(req.body.password, 10);
+
 
         let userSchema = new UserSchema({
             email: req.body.email,
@@ -22,9 +36,24 @@ const signup = async (req, resp) => {
             fullName: req.body.fullName
         });
 
-        userSchema.save()
-            .then(result => resp.status(201).json({'message': 'user saved'}))
-            .catch(error => resp.status(500).json({'message': 'something went wrong', error: error}))
+        await userSchema.save();
+
+        const mailOption={
+            from:process.env.EMAIL_USER,
+            to:req.body.email,
+            subject:'Account creation',
+            text:`account was created........ thank you...`
+        }
+
+        transporter.sendMail(mailOption,(error,info)=>{
+            if(error){
+                console.log(error);
+            }else{
+                console.log('email sent')
+            }
+        });
+
+        resp.status(201).json({'message': 'user saved'});
 
     } catch (e) {
         resp.status(500).json({'message': 'something went wrong', error: e});
@@ -35,28 +64,28 @@ const login = async (req, resp) => {
     //console.log(req.body);
     try {
 
-        const existingUser = await UserSchema.findOne({email:req.body.email});
+        const existingUser = await UserSchema.findOne({email: req.body.email});
 
-        if(!existingUser){
+        if (!existingUser) {
             return resp.status(404).json({'message': 'user not found'});
         }
 
-        const isConfirmed = await bcrypt.compare(req.body.password,existingUser.password);
+        const isConfirmed = await bcrypt.compare(req.body.password, existingUser.password);
 
-        if(!isConfirmed){
+        if (!isConfirmed) {
             return resp.status(401).json({'message': 'password is wrong...'});
         }
 
-       const token =  wt.sign({userId:existingUser._id,email:existingUser.email,fullName:existingUser.fullName},
+        const token = wt.sign({userId: existingUser._id, email: existingUser.email, fullName: existingUser.fullName},
             secret,
             {expiresIn: '5h'});
 
 
-        resp.status(200).json({'token': token,'message':'user logged...'})
+        resp.status(200).json({'token': token, 'message': 'user logged...'})
 
     } catch (e) {
         resp.status(500).json({'message': 'something went wrong', error: e});
     }
 
 }
-module.exports = {signup,login}
+module.exports = {signup, login}
